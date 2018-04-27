70de2de06c946181d548e7108cad2aa6d232754f
neoPix <contact@balandavid.com>
2018-04-27 22:51:10 +0200

    managing CORS

---
+++ simpleWCFApp.WebService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="simpleWCFApp.WebService.Global" Language="C#" %>
---
+++ simpleWCFApp.WebService/Global.asax.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace simpleWCFApp.WebService
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpRequest request = HttpContext.Current.Request;
+            HttpResponse response = HttpContext.Current.Response;
+            response.AddHeader("Access-Control-Allow-Origin", request.Headers.Get("Origin"));
+            if (request.HttpMethod == "OPTIONS")
+            {
+                response.AddHeader("Access-Control-Allow-Methods", "POST, PUT, DELETE, GET");
+                response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization");
+                response.AddHeader("Access-Control-Max-Age", "1728000");
+                response.End();
+            }
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
