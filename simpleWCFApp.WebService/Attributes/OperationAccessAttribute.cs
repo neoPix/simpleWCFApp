c53f531d7228135ef69c3f536b1c331cf84fca8b
neoPix <contact@balandavid.com>
2018-05-01 12:39:32 +0200

    right management and annotations

---
+++ simpleWCFApp.WebService/Attributes/OperationAccessAttribute.cs
@@ -0,0 +1,15 @@
+﻿using simpleWCFApp.Models;
+using System;
+
+namespace simpleWCFApp.WebService.Attributes
+{
+    public class OperationAccessAttribute : Attribute
+    {
+        public OperationAccessAttribute()
+        {
+            this.RequiredAccessLevel = ACCESS_LEVEL.PUBLIC;
+        }
+
+        public ACCESS_LEVEL RequiredAccessLevel { get; set; }
+    }
+}
